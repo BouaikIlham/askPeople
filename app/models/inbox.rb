77dbc1ad76ff2ba9e05191a3b73bc37018ecd01c
ilham bouaik <ibouaik@gmail.com>
2022-10-27 12:13:59 +0100

    install rubocop and fix offenses

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,6 +1,5 @@
 class Inbox < ApplicationRecord
-    validates :name, presence: true
-    validates :name, uniqueness: true
-      validates :name, length: { in: 6..100 }
-
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: 6..100 }
 end
--- test/models/inbox_test.rb
+++ test/models/inbox_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class InboxTest < ActiveSupport::TestCase
   # test "the truth" do
