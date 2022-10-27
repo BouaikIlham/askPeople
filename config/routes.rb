4b34562c963e98782dcfdb14d6b884ad906e4b6c
ilham bouaik <ibouaik@gmail.com>
2022-10-27 10:18:20 +0100

    static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
