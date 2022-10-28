f141b31d4785404d19e14cdd61af41accd9a2777
ilham bouaik <ibouaik@gmail.com>
2022-10-28 16:33:18 +0100

    fix offenses

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -3,32 +3,34 @@ class Inboxes::MessagesController < ApplicationController
   def new
     @message = @inbox.messages.new
   end
+
   def create
     @message = @inbox.messages.new(message_params)
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @inbox, notice: "Message was successfully created." }
+        format.html { redirect_to @inbox, notice: 'Message was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
+
   def destroy
     @message = Message.find(params[:id])
     @message.destroy
     respond_to do |format|
-      format.html { redirect_to @inbox, notice: "Message was successfully destroyed." }
+      format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
     end
   end
 
   private
 
-    def set_inbox
-     @inbox = Inbox.find(params[:inbox_id])
-    end
+  def set_inbox
+    @inbox = Inbox.find(params[:inbox_id])
+  end
 
-    def message_params
-      params.require(:message).permit(:body).merge(user: current_user)
-    end
+  def message_params
+    params.require(:message).permit(:body).merge(user: current_user)
+  end
 end
