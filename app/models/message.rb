b38476d00c88adc14d4ba45721b1c56f90099947
ilham bouaik <ibouaik@gmail.com>
2022-10-30 20:13:44 +0000

    add vote

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :inbox, counter_cache: true
   belongs_to :user
-
   validates :body, presence: true
   validates :body, uniqueness: { scope: :inbox_id }
   validates :body, length: { in: 6..1000 }
+
+
+  acts_as_votable
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :inboxes
   has_many :messages, through: :inboxes
+
+
+  acts_as_voter
+
 end
