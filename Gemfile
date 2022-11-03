4570df97815ed03f34fba3e7a98a72b39f6c00ec
ilham bouaik <ibouaik@gmail.com>
2022-11-03 16:38:41 +0000

    add omniauth

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem 'friendly_id', '~> 5.4.0'
 gem 'meta-tags'
 gem 'pundit', '~> 2.2'
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'omniauth-github', github: 'omniauth/omniauth-github', branch: 'master'
+gem "omniauth-rails_csrf_protection" # for omniauth 2.0
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'ce07772a5b3e52fbc2c2
+', 'd8568c1bd22dda39b83332077e2dacec1a3f1306'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
